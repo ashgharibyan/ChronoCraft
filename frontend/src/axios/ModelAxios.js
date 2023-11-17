355e443859a89655050cb90ce1e4e199868b69a2
Ash Gharibyan <ashghcode@gmail.com>
2023-11-17 15:18:26 -0800

    working task func update

--- frontend/src/axios/ModelAxios.js
+++ frontend/src/axios/ModelAxios.js
@@ -136,7 +136,52 @@ export const listListByFolderAxios = async (setLists, folder_id, navigate) => {
 		}
 	}
 };
+
 export const listTasksByListAxios = async (setTasks, listId, navigate) => {
+	const csrfToken = getCookie("csrftoken");
+
+	try {
+		const response = await axios.get(
+			`http://localhost:8000/api/v1/tasks/?list_id=${listId}`,
+			{
+				withCredentials: true,
+				headers: {
+					"X-CSRFToken": csrfToken,
+				},
+			}
+		);
+		console.log("Successfully fetched tasks data");
+		console.log(response.data.results);
+		setTasks(response.data.results);
+	} catch (err) {
+		if (err.response && err.response.status === 401) {
+			try {
+				const refreshResponse = await axios.post(
+					"http://localhost:8000/api/v1/accounts/dj-rest-auth/token/refresh/",
+					{},
+					{
+						withCredentials: true,
+						headers: {
+							"X-CSRFToken": csrfToken,
+						},
+					}
+				);
+				const newAccessToken = refreshResponse.data.access;
+				localStorage.setItem("jwtToken", newAccessToken);
+				axios.defaults.headers.common["Authorization"] =
+					"Bearer " + newAccessToken;
+				listTasksByListAxios(setTasks, listId, navigate); // retry fetching user data with the new token
+			} catch (refreshErr) {
+				console.error("Error refreshing token", refreshErr);
+				navigate("/login");
+			}
+		} else {
+			console.error("Error fetching tasks list data", err);
+		}
+	}
+};
+
+export const updateTaskAxios = async (listId, navigate) => {
 	const csrfToken = getCookie("csrftoken");
 
 	try {
